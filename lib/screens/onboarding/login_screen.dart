80c9e9415d3c746c7d9b877b07166880b98faaec
y <dkgk2233@gmail.com>
2025-06-17 20:01:21 +0900

    feat: 로그인 페이지 ui

--- lib/screens/onboarding/login_screen.dart
+++ lib/screens/onboarding/login_screen.dart
@@ -9,60 +9,146 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('로그인')),
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            // 이메일 입력
-            TextField(
-              controller: emailController,
-              decoration: InputDecoration(
-                labelText: '이메일',
-                border: OutlineInputBorder(),
-              ),
-              keyboardType: TextInputType.emailAddress,
-            ),
-            SizedBox(height: 16),
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        title: const Text(
+          '로그인',
+          style: TextStyle(
+            fontFamily: 'Pretendard',
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      ),
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/background.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Center(
+          child: SingleChildScrollView(
+            padding: const EdgeInsets.symmetric(horizontal: 24.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SizedBox(height: kToolbarHeight + 200),
 
-            // 비밀번호 입력
-            TextField(
-              controller: passwordController,
-              decoration: InputDecoration(
-                labelText: '비밀번호',
-                border: OutlineInputBorder(),
-              ),
-              obscureText: true,
-            ),
-            SizedBox(height: 24),
+                // 이메일 입력
+                TextField(
+                  controller: emailController,
+                  decoration: InputDecoration(
+                    labelText: '이메일',
+                    labelStyle: const TextStyle(fontFamily: 'Pretendard'),
+                    filled: true,
+                    fillColor: Colors.white70,
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: const BorderSide(color: Colors.white),
+                      borderRadius: BorderRadius.circular(4),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: const BorderSide(color: Colors.white),
+                      borderRadius: BorderRadius.circular(4),
+                    ),
+                  ),
+                  keyboardType: TextInputType.emailAddress,
+                ),
+                const SizedBox(height: 16),
 
-            // 로그인 버튼
-            ElevatedButton(
-              onPressed: () {
-                // 로그인 처리 생략, 단순 이동만
-                Navigator.pushReplacementNamed(context, '/admin');
-              },
-              child: Text('로그인'),
-            ),
-            SizedBox(height: 16),
+                // 비밀번호 입력
+                TextField(
+                  controller: passwordController,
+                  decoration: InputDecoration(
+                    labelText: '비밀번호',
+                    labelStyle: const TextStyle(fontFamily: 'Pretendard'),
+                    filled: true,
+                    fillColor: Colors.white70,
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: const BorderSide(color: Colors.white),
+                      borderRadius: BorderRadius.circular(4),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: const BorderSide(color: Colors.white),
+                      borderRadius: BorderRadius.circular(4),
+                    ),
+                  ),
+                  obscureText: true,
+                ),
+                const SizedBox(height: 24),
 
-            // 비밀번호 변경 페이지 이동
-            TextButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/change-password');
-              },
-              child: Text('비밀번호를 잊으셨나요?'),
-            ),
+                // 로그인 버튼
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushReplacementNamed(context, '/admin');
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.indigo,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(25),
+                    ),
+                    minimumSize: const Size(double.infinity, 55),
+                  ),
+                  child: const Text(
+                    '로그인',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Pretendard',
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 16),
 
-            // 회원가입 페이지 이동
-            TextButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/signup');
-              },
-              child: Text('회원가입'),
+                // 비밀번호 변경
+                TextButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/change-password');
+                  },
+                  child: const Text(
+                    '비밀번호를 잊으셨나요?',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontFamily: 'Pretendard',
+                      color: Color.fromARGB(255, 0, 0, 0),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 160),
+
+                // 회원가입 유도 문구 + 버튼 한 줄 배치
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      '아직 계정이 없으신가요?',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontFamily: 'Pretendard',
+                        color: Color.fromARGB(255, 0, 0, 0),
+                      ),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, '/signup');
+                      },
+                      child: const Text(
+                        '회원가입',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontFamily: 'Pretendard',
+                          fontWeight: FontWeight.bold,
+                          color: Color.fromARGB(255, 0, 0, 0),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
